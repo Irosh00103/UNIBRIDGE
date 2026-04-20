2ad7d0f778ed63bbe126fcbbeb4f8024dc96094c
Nethmika Abeysinghe <abeysinghenethmika@gmail.com>
2026-04-21 03:15:36 +0530

    profile picture upload fix

--- server/server.js
+++ server/server.js
@@ -10,7 +10,8 @@ const app = express();
 
 // Middleware
 app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:5173'], credentials: true }));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
